c2773e997b9c55896239dd411c2a83a70d7f0153
Matilda-02 <Tilymatilda10@gmail.com>
2026-04-29 22:08:21 +0000

    changed database

--- database/db.js
+++ database/db.js
@@ -1,24 +1,11 @@
-require('dotenv').config();
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    require: true,
     rejectUnauthorized: false
   }
-});
-
-pool.on('connect', () => {
-  console.log('Database connected successfully');
-});
-
-pool.on('error', (err) => {
-  console.error('Database error:', err);
 });
 
 module.exports = pool;
